bdb92b299a30377120598c78e21c929d7811ffa8
atosfazi <54644134+atosfazi@users.noreply.github.com>
2024-04-03 21:54:18 +0200

    Add files via upload

--- book_manager/urls.py
+++ book_manager/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import genre_books, authors_list, author_books, top_books
+from .views import genre_books, authors_list, author_books, top_books, random_book
 
 urlpatterns = [
     path('authors/', authors_list, name='authors_list'),
     path('authors/<str:letter>/', authors_list, name='authors_list'),
     path('authors/books/<str:author_name>/', author_books, name='author_books'),
     path('top/', top_books, name='top_books'),
+    path('random/', random_book, name='random_book'),
     path('', genre_books, name='genre_books'),
-    path('<str:genre>/', genre_books, name='genre_books'),
+    path('<str:genre>/', genre_books, name='genre_books')
 ]
